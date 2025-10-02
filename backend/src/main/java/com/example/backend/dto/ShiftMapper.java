f71acc6862a81e257ce56155f3e3415020728309
Daniel-Platz <dani0005@stud.kea.dk>
2025-10-02 12:18:28 +0200

    Implemented ShiftDTO to ShiftMapper

--- backend/src/main/java/com/example/backend/dto/ShiftMapper.java
+++ backend/src/main/java/com/example/backend/dto/ShiftMapper.java
@@ -4,8 +4,32 @@ import com.example.backend.model.Employee;
 import com.example.backend.model.Shift;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class ShiftMapper {
+
+    public ShiftDTO.ShiftDto toDto (Shift shift) {
+        if (shift == null) return null;
+
+        List<EmployeeDTO.EmployeeDto> employees = new ArrayList<>();
+        for (Employee e : shift.getEmployees()) {
+            employees.add(new EmployeeDTO.EmployeeDto(e.getId(), e.getName(), e.getEmail(),e.getPhone()));
+        }
+
+        return new ShiftDTO.ShiftDto(
+                shift.getId(),
+                shift.getStartTime().toString(),
+                shift.getEndTime().toString(),
+                employees
+        );
+    }
+
+
+
+
+
     public ShiftAssignmentDTO.ShiftAssignmentDto toDto (Shift shift, Employee employee ) {
         if (shift == null || employee == null) return null;
         return  new ShiftAssignmentDTO.ShiftAssignmentDto(
