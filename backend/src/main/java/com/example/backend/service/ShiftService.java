a3cf76329999e3755084af5278a9cb2d930ba2f9
Daniel-Platz <dani0005@stud.kea.dk>
2025-10-02 08:30:15 +0200

    Added assignEmployeeToShift to ShiftService interface

--- backend/src/main/java/com/example/backend/service/ShiftService.java
+++ backend/src/main/java/com/example/backend/service/ShiftService.java
@@ -1,4 +1,7 @@
 package com.example.backend.service;
 
+import com.example.backend.dto.ShiftAssignmentDTO;
+
 public interface ShiftService {
+    ShiftAssignmentDTO.ShiftAssignmentDto assignEmployeeToShift(Long shiftId, Long employeeId);
 }
