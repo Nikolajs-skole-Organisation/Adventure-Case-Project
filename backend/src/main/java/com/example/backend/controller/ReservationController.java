8957056b35304c922ab9cd7981990c265a32af54
nial0003 <nial0003@stud.kea.dk>
2025-10-04 15:02:23 +0200

    added te connection between the reservation controller and service

--- backend/src/main/java/com/example/backend/controller/ReservationController.java
+++ backend/src/main/java/com/example/backend/controller/ReservationController.java
@@ -1,5 +1,17 @@
 package com.example.backend.controller;
 
-public class ReservationController
-{
+import com.example.backend.service.ReservationService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+
 }
