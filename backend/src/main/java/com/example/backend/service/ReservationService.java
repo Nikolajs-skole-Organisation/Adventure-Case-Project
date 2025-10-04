b758fba5d31a8694cc761f719919401015f58190
nial0003 <nial0003@stud.kea.dk>
2025-10-04 14:56:09 +0200

    Added all the wanted methods to the ReservationService interface. Also added their bare bones implementation to the ReservationServiceImpl class

--- backend/src/main/java/com/example/backend/service/ReservationService.java
+++ backend/src/main/java/com/example/backend/service/ReservationService.java
@@ -1,4 +1,13 @@
 package com.example.backend.service;
 
+import com.example.backend.model.Reservation;
+
+import java.util.List;
+
 public interface ReservationService {
+    public void createReservation(Reservation reservation);
+    public void cancelReservationById(Long reservationId);
+    public void updateReservation(Long reservationId, Reservation updatedReservation);
+    public Reservation getReservationById(Long id);
+    public List<Reservation> getAllReservations();
 }
--- backend/src/main/java/com/example/backend/service/ReservationServiceImpl.java
+++ backend/src/main/java/com/example/backend/service/ReservationServiceImpl.java
@@ -1,4 +1,36 @@
 package com.example.backend.service;
 
-public class ReservationServiceImpl {
+import com.example.backend.model.Reservation;
+
+import java.util.List;
+
+public class ReservationServiceImpl implements ReservationService{
+    public void createNewReservation(Reservation reservation){
+
+    }
+
+    @Override
+    public void createReservation(Reservation reservation) {
+
+    }
+
+    @Override
+    public void cancelReservationById(Long reservationId) {
+
+    }
+
+    @Override
+    public void updateReservation(Long reservationId, Reservation updatedReservation) {
+
+    }
+
+    @Override
+    public Reservation getReservationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Reservation> getAllReservations() {
+        return List.of();
+    }
 }
