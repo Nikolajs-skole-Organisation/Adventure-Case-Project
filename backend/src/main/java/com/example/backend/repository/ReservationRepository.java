54223618764a3ce2db17003d250813bcaca7bc9b
ntvorup <nach0004@stud.ek.dk>
2025-10-06 16:06:54 +0200

    today view, only active reservations showed by date

--- backend/src/main/java/com/example/backend/repository/ReservationRepository.java
+++ backend/src/main/java/com/example/backend/repository/ReservationRepository.java
@@ -3,8 +3,11 @@ package com.example.backend.repository;
 import com.example.backend.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     Optional<Reservation> findByBookingCode(String bookingCode);
+    List<Reservation> findAllByStartTimeBetweenAndConfirmedFalseOrderByStartTimeAsc(LocalDateTime from, LocalDateTime to);
 }
