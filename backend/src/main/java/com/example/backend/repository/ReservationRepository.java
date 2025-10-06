8d116b3524181388d1289e26698dd231bf792feb
nial0003 <nial0003@stud.kea.dk>
2025-10-07 00:34:27 +0200

    extended the JpaSpecificationExecutor interface on the ReservationRepository which allows us to create dynamic, criteria based queries

--- backend/src/main/java/com/example/backend/repository/ReservationRepository.java
+++ backend/src/main/java/com/example/backend/repository/ReservationRepository.java
@@ -2,12 +2,13 @@ package com.example.backend.repository;
 
 import com.example.backend.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+public interface ReservationRepository extends JpaRepository<Reservation, Long>, JpaSpecificationExecutor<Reservation> {
     Optional<Reservation> findByBookingCode(String bookingCode);
     List<Reservation> findAllByStartTimeBetweenAndConfirmedFalseOrderByStartTimeAsc(LocalDateTime from, LocalDateTime to);
 }
