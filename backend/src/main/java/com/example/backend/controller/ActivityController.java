bdf26128e168e36f2c964a9e66a0c22b77b5627a
MikkelRosenbek <miro0002@stud.kea.dk>
2025-10-08 21:23:47 +0200

    Added @CrossOrigin(origins = {"http://127.0.0.1:5500", "htpp://localhost:5500"}) to ActivityController to build connection from backend to frontend

--- backend/src/main/java/com/example/backend/controller/ActivityController.java
+++ backend/src/main/java/com/example/backend/controller/ActivityController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "htpp://localhost:5500"})
 @RequestMapping("/api/activities")
 public class ActivityController {
 
