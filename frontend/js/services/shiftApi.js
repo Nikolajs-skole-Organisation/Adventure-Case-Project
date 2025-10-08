9ce98f2b4597a01f6e837518ef7368ec7fab5e0e
Daniel-Platz <dani0005@stud.kea.dk>
2025-10-08 21:23:54 +0200

    Created shiftApi

---
+++ frontend/js/services/shiftApi.js
@@ -0,0 +1,9 @@
+import { get } from "../utils/fetchUtils.js";
+
+const BASE_URL = "http://localhost:8080/api";
+const SHIFTS_URL = `${BASE_URL}/shifts`;
+
+export async function getWeeklyShiftOverview(weekStartDate) {
+  const iso = weekStartDate.toISOString().slice(0, 10);
+  return get(`${SHIFTS_URL}/overview?weekStart=${iso}`);
+}
