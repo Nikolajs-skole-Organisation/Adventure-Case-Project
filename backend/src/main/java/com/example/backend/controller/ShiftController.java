385d2be3e94ac92d55291b5d86e8d1065dde9e44
Daniel-Platz <dani0005@stud.kea.dk>
2025-10-09 12:46:49 +0200

    Added method getShiftsBetween to generate weekly shift overview

--- backend/src/main/java/com/example/backend/controller/ShiftController.java
+++ backend/src/main/java/com/example/backend/controller/ShiftController.java
@@ -3,16 +3,21 @@ package com.example.backend.controller;
 import com.example.backend.dto.ShiftAssignmentDTO;
 import com.example.backend.dto.ShiftDTO;
 import com.example.backend.service.ShiftService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -28,6 +33,15 @@ public class ShiftController {
     @GetMapping
     public ResponseEntity<List<ShiftDTO.ShiftDto>> getAllShifts() {
         return ResponseEntity.ok(shiftService.getAllShifts());
+    }
+
+    @GetMapping("/overview")
+    public ResponseEntity<List<ShiftDTO.ShiftDto>> getWeeklyOverview(
+            @RequestParam("weekStart") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)LocalDate weekStart) {
+        LocalDateTime start = weekStart.atStartOfDay();
+        LocalDateTime end = weekStart.plusDays(7).atStartOfDay();
+        List<ShiftDTO.ShiftDto> result = shiftService.getShiftsBetween(start, end);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping("/{shiftId}")
