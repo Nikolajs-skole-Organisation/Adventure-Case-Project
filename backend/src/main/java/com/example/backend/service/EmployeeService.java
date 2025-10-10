dad7c9df7706180373827e9071496baae41faa09
Daniel-Platz <dani0005@stud.kea.dk>
2025-10-10 08:05:38 +0200

    Added simple getAllEmployees to backend, which i can use to chose between employees when assigning to a shift

--- backend/src/main/java/com/example/backend/service/EmployeeService.java
+++ backend/src/main/java/com/example/backend/service/EmployeeService.java
@@ -2,6 +2,9 @@ package com.example.backend.service;
 
 import com.example.backend.dto.EmployeeDTO;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDTO.EmployeeDto login(String email, String password);
+    List<EmployeeDTO.EmployeeDto> getAllEmployees();
 }
